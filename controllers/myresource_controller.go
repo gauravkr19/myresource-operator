5e56d5fafb23cd4f25fbe620806f53e0866af9e2
gauravkr19 <gauravkr19@gmail.com>
2023-09-24 10:44:04 +0000

    operator to create workloads, pvc

---
+++ controllers/myresource_controller.go
@@ -0,0 +1,425 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	gauravkr19devv1alpha1 "github.com/gauravkr19/myresource/api/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// var log = log.Log.WithName("my-resource-controller")
+
+// MyResourceReconciler reconciles a MyResource object
+type MyResourceReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=gauravkr19.dev,resources=myresources,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=gauravkr19.dev,resources=myresources/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=gauravkr19.dev,resources=myresources/finalizers,verbs=update
+
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the MyResource object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
+
+func (r *MyResourceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := log.FromContext(ctx)
+
+	// Fetch the MyResource custom resource
+	myResource := &gauravkr19devv1alpha1.MyResource{}
+	if err := r.Client.Get(ctx, req.NamespacedName, myResource); err != nil {
+		logger.Error(err, "Failed to fetch MyResource")
+		return ctrl.Result{}, err
+	}
+
+	// Create or update the PVC
+	pvcName := myResource.Name + "-db" + "-pvc"
+	if err := r.createPVC(ctx, myResource, pvcName); err != nil {
+		logger.Error(err, "Failed to create or update PVC")
+		return ctrl.Result{}, err
+	}
+
+	// Create or update the Secret for Deployment
+	secretName := myResource.Name + "-app" + "-secret"
+	secKey := "somekey"
+	secVal := "someval"
+	if err := r.createSecret(ctx, myResource, secretName, secKey, secVal); err != nil {
+		logger.Error(err, "Failed to create or update Secret")
+		return ctrl.Result{}, err
+	}
+	// Create or update the Secret for Statefulset
+	secretName = myResource.Name + "-db" + "-secret"
+	if err := r.createSecret(ctx, myResource, secretName, secKey, secVal); err != nil {
+		logger.Error(err, "Failed to create or update Secret")
+		return ctrl.Result{}, err
+	}
+
+	// Create Service for Deployment
+	deploymentServiceName := myResource.Name + "-app" + "-deployment-service"
+	if err := r.createServiceApp(ctx, myResource, deploymentServiceName); err != nil {
+		logger.Error(err, "Failed to create Deployment Service")
+		return ctrl.Result{}, err
+	}
+
+	// Create Service for StatefulSet
+	statefulSetServiceName := myResource.Name + "-db" + "-statefulset-service"
+	if err := r.createServiceDB(ctx, myResource, statefulSetServiceName); err != nil {
+		logger.Error(err, "Failed to create StatefulSet Service")
+		return ctrl.Result{}, err
+	}
+
+	// Reconcile the Deployment
+	if err := r.createOrUpdateDeployment(ctx, myResource); err != nil {
+		logger.Error(err, "Failed to reconcile Deployment")
+		return ctrl.Result{}, err
+	}
+
+	// Reconcile the StatefulSet
+	if err := r.createOrUpdateStatefulSet(ctx, myResource); err != nil {
+		logger.Error(err, "Failed to reconcile StatefulSet")
+		return ctrl.Result{}, err
+	}
+
+	logger.Info("Reconciliation completed successfully")
+	return ctrl.Result{}, nil
+}
+
+// DEPLOYMENT
+func (r *MyResourceReconciler) createOrUpdateDeployment(ctx context.Context, myResource *gauravkr19devv1alpha1.MyResource) error {
+	logger := log.FromContext(ctx)
+
+	logger.Info("Reconciling Deployment...")
+
+	// Define your Deployment based on myResource specifications
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      myResource.Name + "-deployment",
+			Namespace: myResource.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &myResource.Spec.DeploymentReplicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"app": myResource.Name + "-app"},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{"app": myResource.Name + "-app"},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  myResource.Name + "-app" + "-container",
+							Image: myResource.Spec.Image,
+							Env: []corev1.EnvVar{{
+								Name: "POSTGRES_PASSWORD",
+								ValueFrom: &corev1.EnvVarSource{
+									SecretKeyRef: &corev1.SecretKeySelector{
+										LocalObjectReference: corev1.LocalObjectReference{
+											Name: myResource.Name + "-app" + "-secret",
+										},
+										Key: "POSTGRES_PASSWORD",
+									},
+								}},
+								{Name: "POSTGRES_DB",
+									Value: "alertsnitch",
+								},
+								{Name: "POSTGRES_USER",
+									Value: "alertsnitch",
+								},
+								{Name: "POSTGRES_HOST",
+									Value: "hostname",
+								}}, // End of Env listed values and Env definition
+						},
+					},
+					// Volumes: []corev1.Volume{
+					// 	{
+					// 		Name: "secret-volume",
+					// 		VolumeSource: corev1.VolumeSource{
+					// 			Secret: &corev1.SecretVolumeSource{
+					// 				SecretName: secretName,
+					// 			},
+					// 		},
+					// 	},
+					// },
+				},
+			},
+		},
+	}
+
+	// Use the custom CreateOrUpdate function to create or update the Deployment
+	if err := r.CreateOrUpdate(ctx, deployment); err != nil {
+		logger.Error(err, "Failed to create or update Deployment")
+		return err
+	}
+	ctrl.SetControllerReference(myResource, deployment, r.Scheme)
+	logger.Info("Deployment reconciliation completed successfully")
+	return nil
+}
+
+func (r *MyResourceReconciler) CreateOrUpdate(ctx context.Context, obj client.Object) error {
+	logger := log.FromContext(ctx)
+
+	logger.Info("Creating or updating resource...")
+
+	// Try to create the resource. If it already exists, this will result in an error.
+	err := r.Client.Create(ctx, obj)
+	if err != nil && errors.IsAlreadyExists(err) {
+		// The resource already exists; try to update it.
+		err = r.Client.Update(ctx, obj)
+	}
+
+	if err != nil {
+		logger.Error(err, "Failed to create or update resource")
+	} else {
+		logger.Info("Resource creation or update completed successfully")
+	}
+
+	return err
+}
+
+func (r *MyResourceReconciler) createSecret(ctx context.Context, myResource *gauravkr19devv1alpha1.MyResource, secretName string, secKey string, secVal string) error {
+	logger := log.FromContext(ctx)
+	secKey = "POSTGRES_PASSWORD"
+	secVal = "alertsnitch"
+
+	logger.Info("Creating Secret...")
+
+	sec := make(map[string]string)
+	sec[secKey] = secVal
+
+	// Define the Secret based on myResource specifications
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{Name: secretName, Namespace: myResource.Namespace},
+		Type:       corev1.SecretTypeOpaque,
+		StringData: sec,
+	}
+	// Create the Secret
+	if err := r.Client.Create(ctx, secret); err != nil {
+		logger.Error(err, "Failed to create Secret")
+		return err
+	}
+
+	// Set the owner reference for the
+	ctrl.SetControllerReference(myResource, secret, r.Scheme)
+
+	logger.Info("Secret creation completed successfully")
+	return nil
+}
+
+// STATEFULSET
+func (r *MyResourceReconciler) createOrUpdateStatefulSet(ctx context.Context, myResource *gauravkr19devv1alpha1.MyResource) error {
+	logger := log.FromContext(ctx)
+
+	logger.Info("Reconciling StatefulSet...")
+
+	// Define your StatefulSet based on myResource specifications
+	statefulSet := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      myResource.Name + "-db" + "-statefulset",
+			Namespace: myResource.Namespace,
+		},
+		Spec: appsv1.StatefulSetSpec{
+			Replicas: &myResource.Spec.StatefulSetReplicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"app": myResource.Name + "-db"},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{"app": myResource.Name + "-db"},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  myResource.Name + "-db" + "-container",
+							Image: "docker.io/gauravkr19/postgres-data",
+							// Image: myResource.Spec.ImageDB,
+							Env: []corev1.EnvVar{{
+								Name: "POSTGRES_PASSWORD",
+								ValueFrom: &corev1.EnvVarSource{
+									SecretKeyRef: &corev1.SecretKeySelector{
+										LocalObjectReference: corev1.LocalObjectReference{
+											Name: myResource.Name + "-db" + "-secret",
+										},
+										Key: "POSTGRES_PASSWORD",
+									},
+								}},
+								{Name: "POSTGRES_DB",
+									Value: "alertsnitch",
+								},
+								{Name: "POSTGRES_USER",
+									Value: "alertsnitch",
+								},
+								{Name: "PGDATA",
+									Value: "/var/lib/postgresql/data/pgdata",
+								}}, // End of Env listed values and Env definition
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "database-volume",
+									MountPath: "/var/lib/postgresql/data",
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "database-volume",
+							VolumeSource: corev1.VolumeSource{
+								PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+									ClaimName: myResource.Name + "-db" + "-pvc",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// Use the custom CreateOrUpdate function to create or update the StatefulSet
+	if err := r.CreateOrUpdate(ctx, statefulSet); err != nil {
+		logger.Error(err, "Failed to create or update StatefulSet")
+		return err
+	}
+	ctrl.SetControllerReference(myResource, statefulSet, r.Scheme)
+	logger.Info("StatefulSet reconciliation completed successfully")
+	return nil
+}
+
+func (r *MyResourceReconciler) createPVC(ctx context.Context, myResource *gauravkr19devv1alpha1.MyResource, pvcName string) error {
+	logger := log.FromContext(ctx)
+
+	logger.Info("Creating PVC...")
+
+	// Define the PVC based on myResource specifications
+	pvc := &corev1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      pvcName,
+			Namespace: myResource.Namespace,
+		},
+		Spec: corev1.PersistentVolumeClaimSpec{
+			AccessModes: []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
+			Resources: corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceStorage: resource.MustParse(myResource.Spec.PVCSize),
+				},
+			},
+		},
+	}
+	// Create the PVC
+	if err := r.Client.Create(ctx, pvc); err != nil {
+		logger.Error(err, "Failed to create PVC")
+		return err
+	}
+
+	logger.Info("PVC creation completed successfully")
+	return nil
+}
+
+func (r *MyResourceReconciler) createServiceApp(ctx context.Context, myResource *gauravkr19devv1alpha1.MyResource, serviceName string) error {
+	logger := log.FromContext(ctx)
+	logger.Info("Creating Service...")
+
+	// Define your Service based on myResource specifications
+	service := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      serviceName,
+			Namespace: myResource.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{"app": myResource.Name + "-app"},
+			Ports: []corev1.ServicePort{
+				{
+					Port: 80,
+					Name: "http",
+				},
+			},
+		},
+	}
+	// Set the owner reference for the Service
+	ctrl.SetControllerReference(myResource, service, r.Scheme)
+
+	// Create the Service
+	if err := r.Client.Create(ctx, service); err != nil {
+		logger.Error(err, "Failed to create Service")
+		return err
+	}
+	logger.Info("Service creation completed successfully")
+	return nil
+}
+func (r *MyResourceReconciler) createServiceDB(ctx context.Context, myResource *gauravkr19devv1alpha1.MyResource, serviceName string) error {
+	logger := log.FromContext(ctx)
+	logger.Info("Creating Service...")
+
+	// Define your Service based on myResource specifications
+	service := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      serviceName,
+			Namespace: myResource.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{"app": myResource.Name + "-db"},
+			Ports: []corev1.ServicePort{
+				{
+					Port: 5432,
+					Name: "tcp",
+				},
+			},
+		},
+	}
+	// Set the owner reference for the Service
+	ctrl.SetControllerReference(myResource, service, r.Scheme)
+
+	// Create the Service
+	if err := r.Client.Create(ctx, service); err != nil {
+		logger.Error(err, "Failed to create Service")
+		return err
+	}
+	logger.Info("Service creation completed successfully")
+	return nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *MyResourceReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&gauravkr19devv1alpha1.MyResource{}).
+		Owns(&appsv1.Deployment{}).
+		Owns(&appsv1.StatefulSet{}).
+		Owns(&corev1.PersistentVolumeClaim{}).
+		Complete(r)
+}
